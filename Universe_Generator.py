a486b03cbfa01561ce8cf06be0b79d8319312b92
FrozenGoose12 <jjarwalker@gmail.com>
2025-12-13 20:27:26 -0500

    Black Hole Addition Universe_Generator.py

--- Universe_Generator.py
+++ Universe_Generator.py
@@ -15,41 +15,28 @@ def exit_check():
             sys.exit()
 
 def Shooting_Star(Shooting_Star_rng):
-    star_list = []
-
+    color = (210, 230, 255)
     x = Shooting_Star_rng.randint(0, screen.get_width())
     y = Shooting_Star_rng.randint(0, screen.get_height())
 
     dx = Shooting_Star_rng.uniform(-5, 5)
     dy = Shooting_Star_rng.uniform(-5, 5)
 
-    shoot_length = Shooting_Star_rng.randint(50, 150)
+    shoot_length = Shooting_Star_rng.randint(1000, 1500)
 
     for i in range(shoot_length):
         try:
             previous_color = screen.get_at((int(x), int(y)))
         except IndexError:
             break
-        star_list.append((x, y, previous_color))
+
+        pygame.draw.circle(screen, color, (int(x), int(y)), 1)
+        pygame.display.flip()
+        pygame.time.delay(3)
+
+        pygame.draw.circle(screen, previous_color, (int(x), int(y)), 1)
         x += dx
         y += dy
-
-
-    for light in star_list:
-        trail = Shooting_Star_rng.randint(1, 2)
-        if trail == 1:
-            color = (255, 255, 255)
-        else:
-            color = (210, 230, 255)
-        pygame.draw.circle(screen, color, (int(light[0]), int(light[1])), 1)
-        pygame.display.flip()
-        pygame.time.delay(1)
-
-    for light in star_list:
-        pygame.draw.circle(screen, light[2], (int(light[0]), int(light[1])), 1)
-        pygame.display.flip()
-        pygame.time.delay(1)
-
 
 def CosmosGenerator(Cosmos_rng):
     WHITE = (255, 255, 255)
@@ -145,6 +132,131 @@ def NebulaGenerator(center, nebula_radius, clouds, red_range = (0, 255), green_r
             pygame.draw.circle(screen, WHITE, (starx, stary), starsize)
 
     pygame.display.flip()
+
+
+def BlackHoleGenerator(bh_radius, rotation, center):
+    size = bh_radius
+    size2 = bh_radius
+    bigtheta = rotation
+    a = 4
+    b = 3
+
+    l1 = (234, 228, 240)
+    l2 = (221, 212, 222)
+    l3 = (231, 224, 236)
+    l4 = (193, 172, 175)
+
+    if bh_radius >= 200:
+        hole_r = int(size2 * 2.15)
+        fr_width = 5
+    elif bh_radius >= 40:
+        hole_r = int(size2 * 2.2)
+        fr_width = 4
+    elif bh_radius >= 23:
+        hole_r = int(size2 * 2.3)
+        fr_width = 4
+    elif bh_radius >= 10:
+        hole_r = int(size2 * 2.4)
+        fr_width = 4
+    else:
+        hole_r = int(size2 * 2.6)
+        fr_width = 4
+
+    # glow
+    glow_diameter = 15 * bh_radius
+    for i in range(30):
+        black_hole_glow_surface = pygame.Surface((glow_diameter, glow_diameter), pygame.SRCALPHA)
+        black_hole_glow_surface.set_alpha(3)
+        pygame.draw.circle(black_hole_glow_surface, l4, (glow_diameter // 2, glow_diameter // 2), glow_diameter // 2)
+        screen.blit(black_hole_glow_surface, (center[0] - glow_diameter // 2, center[1] - glow_diameter // 2))
+        glow_diameter -= glow_diameter * .03
+
+    pygame.display.flip()
+
+    # outer top
+    theta = 0
+    for i in range(size):
+        if i > size * .5:
+            color = l1
+        elif i > size * .3:
+            color = l2
+        elif i > size * .1:
+            color = l3
+        else:
+            color = l4
+
+        while math.pi >= theta:
+            r = size * (a + b * ((abs(math.cos(2 * theta))) ** 2))
+            x = (r * math.cos(theta))
+            y = (r * math.sin(theta))
+
+            y *= .3
+
+            rotatedx = center[0] - (math.cos(bigtheta) * x + (-1 * math.sin(bigtheta) * y))
+            rotatedy = center[1] - (math.sin(bigtheta) * x + (math.cos(bigtheta) * y))
+
+            pygame.draw.circle(screen, color, (rotatedx, rotatedy), fr_width)
+            theta += .01
+        pygame.display.flip()
+
+        size -= 1
+        theta = 0
+
+
+    # inner hole
+
+    pygame.draw.circle(screen, (0, 0, 0), (center[0], center[1]), hole_r, 0) # black hole
+    pygame.draw.circle(screen, (255, 240, 240), (center[0]+1, center[1]-1), hole_r - (hole_r // 8), 1) # inner lense
+
+    warp = hole_r // 4
+    for disk in range(warp): # circular disks
+        if disk > warp * .5:
+            color = l4
+            rect = pygame.Rect(center[0] - hole_r, center[1] - hole_r, hole_r * 2, hole_r * 2)
+            pygame.draw.arc(screen, color, rect,  (.8 * math.pi)/6 - bigtheta, (5.1 * math.pi)/6 - bigtheta, 1) # top l4
+            pygame.draw.arc(screen, color, rect, math.pi - bigtheta, 2 * math.pi - bigtheta, 1) # bottom l4
+            hole_r += 1
+            continue
+        elif disk > warp * .3:
+            color = l3
+        elif disk > warp * .1:
+            color = l2
+            rect = pygame.Rect(center[0] - hole_r, center[1] - hole_r, hole_r * 2, hole_r * 2)
+            pygame.draw.arc(screen, color, rect, (.8 * math.pi) / 6 - bigtheta, (5.1 * math.pi) / 6 - bigtheta,1)  # top l4
+            pygame.draw.arc(screen, color, rect, math.pi - bigtheta, 2 * math.pi - bigtheta, 1)  # bottom l4
+            hole_r += 1
+            continue
+        else:
+            color = l1
+
+        hole_r += 1
+        pygame.draw.circle(screen, color, (center[0], center[1]), hole_r, 3)
+    pygame.display.flip()
+
+    # mid ring
+    for i in range(size2 - int(size2//3.25)):
+        if i > size2  * .5:
+            color = l1
+        elif i > size2  * .3:
+            color = l2
+        elif i > size2  * .1:
+            color = l3
+        else:
+            color = l4
+
+        mringr = size2 * (a + b)
+        for x in range(int(-mringr), int(mringr) + 1):
+            y = math.sqrt((mringr ** 2) - (x ** 2))
+            y *= .13
+
+            rotatedx = center[0] + (math.cos(bigtheta) * x + (-1 * math.sin(bigtheta) * y))
+            rotatedy = center[1] + (math.sin(bigtheta) * x + (math.cos(bigtheta) * y))
+
+            pygame.draw.circle(screen, color, (rotatedx, rotatedy), fr_width)
+        pygame.display.flip()
+        size2 -= 1
+
+
 
 def SpiralGalaxyGenerator(max_radius = 1000, turns = 3 * math.pi, bigtheta = random.uniform(0, math.pi * 2), density_variation = 3, x_squish = 1, y_squish = .3, bsub = .02, red_range = (0, 255), green_range = (0, 255), blue_range = (0, 255), gas_alpha = 7, center = (-1, -1)):
     if center == (-1, -1):
@@ -288,6 +400,7 @@ def SpiralGalaxyGenerator(max_radius = 1000, turns = 3 * math.pi, bigtheta = ran
             break
 
 
+
 def Universe_Generator(Universe_Seed = random.randint(0, 10000), debug = 0):
 
     Cosmo_rng = random.Random(Universe_Seed + 1) #pre determined seemingly random lists (RNG)
@@ -308,9 +421,9 @@ def Universe_Generator(Universe_Seed = random.randint(0, 10000), debug = 0):
         blue_range_start = Nebula_rng.randint(10, 130)
         blue_range = (blue_range_start, blue_range_start + 125)
 
-        proportional = Nebula_rng.randint(100, 400)
-        NebulaGenerator(
-                (Nebula_rng.randint(0, screen.get_width()), Nebula_rng.randint(0, screen.get_height())), #center
+        proportional = Nebula_rng.randint(100, 500)
+        nebula_center = (Nebula_rng.randint(0, screen.get_width()), Nebula_rng.randint(0, screen.get_height()))
+        NebulaGenerator(nebula_center, #center
                         proportional, # nebula_radius
                         proportional, # clouds
                         red_range, green_range, blue_range, # colors
@@ -318,86 +431,132 @@ def Universe_Generator(Universe_Seed = random.randint(0, 10000), debug = 0):
                         )
 
         if debug == 1:
-            print(f"NEBULA_NUMBER: {nebula+1}\nNEBULA_COLOR_RANGES: ({red_range}, {green_range}, {blue_range})\nNEBULA_RADIUS and CLOUDS: {proportional}\n")
+            print(f"NEBULA_NUMBER: {nebula+1}\nNEBULA_CENTER: {nebula_center}\nNEBULA_COLOR_RANGES: ({red_range}, {green_range}, {blue_range})\nNEBULA_RADIUS and CLOUDS: {proportional}\n")
 
-    global Galaxy_rng
-    Galaxy_rng = random.Random(Universe_Seed + 3) #pre determined seemingly random lists (RNG)
-    for galaxy in range(Galaxy_rng.randint(1, 5)):
-        giga_galaxy = Galaxy_rng.randint(0, 10)
-        if giga_galaxy == 7:
-            max_radius = Galaxy_rng.randint(480, 1000)
-            greed = True
+
+    Black_Hole_rng = random.Random(Universe_Seed + 5)  # pre determined seemingly random lists (RNG)
+
+    singularity = Black_Hole_rng.randint(0, 7)
+    if singularity == 0:
+        density = Black_Hole_rng.randint(0, 7)
+        if density == 0:
+            black_hole_radius = Black_Hole_rng.randint(100, 200)
         else:
-            max_radius = Galaxy_rng.randint(30, 479)
-            greed = False
+            black_hole_radius = Black_Hole_rng.randint(10, 99)
 
-        if max_radius < 100:
-            turns = math.pi * (2 + Galaxy_rng.uniform(.4, .9))
-            density_variation = Galaxy_rng.randint(1, 2)
+        if black_hole_radius > 50:
+            supermassive = True
         else:
-            turns = Galaxy_rng.uniform(3.5 * math.pi, 6 * math.pi)
-            density_variation = Galaxy_rng.randint(2, 10)
+            supermassive = False
 
-        bigtheta = Galaxy_rng.uniform(0, math.pi * 2)
-        p = 1
-        q = Galaxy_rng.uniform(.1, 1)
-        bsub = .02
-
-        galaxy_color = Galaxy_rng.randint(1, 5)
-        if galaxy_color == 1: # bluish
-            red_range = (0, 100)
-            green_range = (40, 120)
-            blue_range = (150, 230)
-        elif galaxy_color == 2: # magenta
-            red_range = (120, 200)
-            green_range = (0, 80)
-            blue_range = (150, 255)
-        elif galaxy_color == 3: # tealish
-            red_range = (0, 180)
-            green_range = (120, 255)
-            blue_range = (150, 255)
-        elif galaxy_color == 4: # yellowish orange
-            red_range = (200, 255)
-            green_range = (150, 220)
-            blue_range = (120, 200)
-        elif galaxy_color == 5: # warm dust
-            red_range = (245, 255)
-            green_range = (215, 225)
-            blue_range = (175, 185)
-
-
-        gas_alpha = Galaxy_rng.randint(3, 25)
-        center = (Galaxy_rng.randint(0, screen.get_width()), Galaxy_rng.randint(0, screen.get_height()))
-
-        SpiralGalaxyGenerator(max_radius, # max_radius
-                              turns, # turns
-                              bigtheta, # bigtheta
-                              density_variation, # density_variation
-                              p, q, #x and y squish constants (p and q)
-                              bsub, #b (theta coefficient) subtraction increment
-                              red_range, green_range, blue_range, # galaxy gas colors
-                              gas_alpha, #gas_color and gas_alpha
-                              center)
+        black_hole_center = (Black_Hole_rng.randint(0, screen.get_width()),
+                             Black_Hole_rng.randint(0, screen.get_height()))
+        black_hole_rotation = Black_Hole_rng.uniform(-math.pi/2, math.pi / 2)
+        BlackHoleGenerator(black_hole_radius, black_hole_rotation, black_hole_center)
 
         if debug == 1:
-            print(f"GALAXY NUMBER: {galaxy+1}\nMAX_RADIUS: {max_radius}\nTURNS: {turns}\nBIG_THETA: {bigtheta}\nDENSITY_VARIATION: {density_variation}\n"
-              f"XY SQUISH: {p}, {q}\nB_INCREMENT: {bsub}\n"
-              f"GAS_COLORS and GAS_ALPHA: ({red_range}, {green_range}, {blue_range}), {gas_alpha}\nGAS CENTER: {center}\nGREED: {greed}\n")
+            print(
+                f"BLACK_HOLE_RADIUS: {black_hole_radius}\nBLACK_HOLE_CENTER: {black_hole_center}\nBLACK_HOLE_ROTATION: {black_hole_rotation}\n")
+    else:
+        supermassive = False
 
-        if greed:
-            break
+    if not supermassive:
+        global Galaxy_rng
+        Galaxy_rng = random.Random(Universe_Seed + 3) # pre determined seemingly random lists (RNG)
+        for galaxy in range(Galaxy_rng.randint(1, 5)):
+            giga_galaxy = Galaxy_rng.randint(0, 10)
+            if giga_galaxy == 7:
+                max_radius = Galaxy_rng.randint(480, 1000)
+                greed = True
+            elif 1 <= giga_galaxy <= 6:
+                max_radius = Galaxy_rng.randint(201, 479)
+                greed = False
+            else:
+                max_radius = Galaxy_rng.randint(30, 200)
+                greed = False
+
+            if max_radius < 100:
+                turns = math.pi * (2 + Galaxy_rng.uniform(.4, .9))
+                density_variation = Galaxy_rng.randint(1, 2)
+            else:
+                turns = Galaxy_rng.uniform(3.5 * math.pi, 6 * math.pi)
+                density_variation = Galaxy_rng.randint(2, 10)
+
+            bigtheta = Galaxy_rng.uniform(0, math.pi * 2)
+            p = 1
+            q = Galaxy_rng.uniform(.1, 2)
+            bsub = .02
+
+            galaxy_color = Galaxy_rng.randint(1, 5)
+            if galaxy_color == 1: # bluish
+                red_range = (0, 100)
+                green_range = (40, 120)
+                blue_range = (120, 230)
+                gas_alpha = Galaxy_rng.randint(3, 10)
+            elif galaxy_color == 2: # magenta
+                red_range = (120, 200)
+                green_range = (0, 80)
+                blue_range = (150, 255)
+                gas_alpha = Galaxy_rng.randint(3, 13)
+            elif galaxy_color == 3: # tealish
+                red_range = (0, 180)
+                green_range = (120, 255)
+                blue_range = (150, 255)
+                gas_alpha = Galaxy_rng.randint(3, 10)
+            elif galaxy_color == 4: # yellowish orange
+                red_range = (200, 255)
+                green_range = (150, 220)
+                blue_range = (120, 200)
+                gas_alpha = Galaxy_rng.randint(1, 5)
+            elif galaxy_color == 5: # warm dust
+                red_range = (245, 255)
+                green_range = (215, 225)
+                blue_range = (175, 185)
+                gas_alpha = Galaxy_rng.randint(2, 5)
 
 
-    Shooting_Star_rng = random.Random(Universe_Seed + 4)
+            center = (Galaxy_rng.randint(0, screen.get_width()), Galaxy_rng.randint(0, screen.get_height()))
+
+            SpiralGalaxyGenerator(max_radius, # max_radius
+                                  turns, # turns
+                                  bigtheta, # bigtheta
+                                  density_variation, # density_variation
+                                  p, q, #x and y squish constants (p and q)
+                                  bsub, #b (theta coefficient) subtraction increment
+                                  red_range, green_range, blue_range, # galaxy gas colors
+                                  gas_alpha, #gas_color and gas_alpha
+                                  center)
+
+            if debug == 1:
+                print(f"GALAXY NUMBER: {galaxy+1}\nMAX_RADIUS: {max_radius}\nTURNS: {turns}\nBIG_THETA: {bigtheta}\nDENSITY_VARIATION: {density_variation}\n"
+                  f"XY SQUISH: {p}, {q}\nB_INCREMENT: {bsub}\n"
+                  f"GAS_COLORS and GAS_ALPHA: ({red_range}, {green_range}, {blue_range}), {gas_alpha}\nGAS CENTER: {center}\nGREED: {greed}\n")
+
+            if greed:
+                break
+
+
+
+
+    Shooting_Star_rng = random.Random(Universe_Seed + 4) # pre determined seemingly random lists (RNG)
     while True:
         exit_check()
-        shoot = Shooting_Star_rng.randint(1, 1000000)
+        shoot = Shooting_Star_rng.randint(1, 5000000)
         if shoot == 7:
             Shooting_Star(Shooting_Star_rng)
 
 
 
 if __name__ == "__main__":
+    # seed = 0
+    # while seed != "":
+    #     seed = input("Enter Universe Seed (Hit Enter For Random Seed): ")
+    #     try:
+    #         seed = int(seed)
+    #         break
+    #     except ValueError:
+    #         continue
+
     pygame.init()
     # screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     screen_info = pygame.display.Info()
@@ -408,5 +567,12 @@ if __name__ == "__main__":
     clock.tick(60)
     screen.fill((0, 0, 0))
 
-    # Universe_Generator(random.randint(0,1000000000000), 1)
-    # Universe_Generator(11123123, 1)
+    # if seed == "":
+    #     Universe_Generator(random.randint(0,1000000000000), 1)
+    # else:
+    #     Universe_Generator(seed, 1)
+
+
+    # giga seed: 114329036248
+    Universe_Generator(random.randint(0, 1000000000000), 1)
+    # Universe_Generator(0)
